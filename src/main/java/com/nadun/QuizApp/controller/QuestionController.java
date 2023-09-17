863b0684d1ff4effe0da569ddf5e7a0c1d8195d1
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2023-09-17 23:49:56 +0530

    Created simple Quiz App backend

---
+++ src/main/java/com/nadun/QuizApp/controller/QuestionController.java
@@ -0,0 +1,32 @@
+package com.nadun.QuizApp.controller;
+
+
+import com.nadun.QuizApp.model.Question;
+import com.nadun.QuizApp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestion(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+
+    }
+}
